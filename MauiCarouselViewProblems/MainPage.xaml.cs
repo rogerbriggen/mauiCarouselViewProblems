edc49e270dd51eb1e95c0b9db0ae9ae75a81a165
Roger Briggen <roger.briggen@bluewin.ch>
2023-02-14 15:34:20 +0100

    Show workaround for Windows

--- MauiCarouselViewProblems/MainPage.xaml.cs
+++ MauiCarouselViewProblems/MainPage.xaml.cs
@@ -13,19 +13,24 @@ namespace MauiCarouselViewProblems
             animalsCollection.Add(new Animal { Name = "Dog", Birthday = DateTime.Now.ToString() });
         }
 
-        public ObservableCollection<Animal> animalsCollection { get; } 
+        public ObservableCollection<Animal> animalsCollection { get; set; } 
 
         private void OnCreateNewListBtnClicked(object sender, EventArgs e)
         {
-            animalsCollection.Clear();
+            //animalsCollection.Clear();
+            animalsCollection = new ObservableCollection<Animal>();
             animalsCollection.Add(new Animal { Name = "Monkey", Birthday = DateTime.Now.ToString() });
+            OnPropertyChanged(nameof(animalsCollection));
         }
 
         private void OnCreateNewListBtn2Clicked(object sender, EventArgs e)
         {
-            animalsCollection.Clear();
+            //animalsCollection.Clear();
+            animalsCollection = new ObservableCollection<Animal>();
+            
             animalsCollection.Add(new Animal { Name = "Monkey", Birthday = DateTime.Now.ToString() });
             animalsCollection.Add(new Animal { Name = "Bird", Birthday = DateTime.Now.ToString() });
+            OnPropertyChanged(nameof(animalsCollection));
         }
     }
 }
